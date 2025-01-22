c57174e75a78c7ddceb955970b0fb679ee657166
“ederjgb94” <ederjbg94@gmail.com>
2025-01-22 12:31:07 -0600

    Remove Test module bindings, controller, and view files

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
-import '../modules/test/bindings/test_binding.dart';
-import '../modules/test/views/test_view.dart';
 
 part 'app_routes.dart';
 
@@ -17,11 +15,6 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.TEST,
-      page: () => const TestView(),
-      binding: TestBinding(),
     ),
   ];
 }
